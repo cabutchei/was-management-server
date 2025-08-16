2aa2501c4dab2e7dddbb5cb9ff007a3c0fef5df3
cabutchei <luropa_paz@hotmail.com>
2025-08-16 10:13:58 -0300

    first startServer integrated test

--- src/com/cabutchei/ServerProcess.java
+++ src/com/cabutchei/ServerProcess.java
@@ -4,6 +4,8 @@ package com.cabutchei;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * The ServerProcess class manages the lifecycle of server processes.
@@ -15,14 +17,24 @@ public class ServerProcess {
     private Map<String, Process> processes = new HashMap<>();
 
 
-    public void startServer(String serverName) {
-        String command = "C:/Desenvolvimento/IBM/WebSphere/AppServer_8_5/bin/startServer.bat ";
-        ProcessBuilder processBuilder = new ProcessBuilder(command, serverName);
-        try {
-            processBuilder.start();
-            System.out.println("Server " + serverName + " started.");
+    public void startServer(String serverId) {
+        // String command = "C:/Desenvolvimento/IBM/WebSphere/AppServer_8_5/bin/startServer.bat";
+        Path scriptPath = Paths.get(".").getParent().getParent().getParent()
+            .resolve("script").resolve("startServer.bat").toAbsolutePath();
+        // ProcessBuilder processBuilder = new ProcessBuilder(command, "-profileName", "AppSrv03", "-script", scriptPath.toString(), "server1");
+        // try {
+        //     processBuilder.start();
+        //     System.out.println("Server " + serverId + " started.");
+        // } catch (Exception e) {
+        //     System.err.println("Failed to start server " + serverId + ": " + e.getMessage());
+        // }
+        ProcessBuilder serverProcessBuilder = new ProcessBuilder(scriptPath.toString());
+        try{
+            serverProcessBuilder.start();
+            processes.put(serverId, serverProcessBuilder.start());
+            System.out.println("Server " + serverId + " started.");
         } catch (Exception e) {
-            System.err.println("Failed to start server " + serverName + ": " + e.getMessage());
+            System.err.println("Failed to start server " + serverId + ": " + e.getMessage());
         }
         // ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", serverName + ".jar");
         // try {
@@ -34,14 +46,14 @@ public class ServerProcess {
         // }
     }
 
-    public void stopServer(String serverName) {
-        String command = "C:/Desenvolvimento/IBM/WebSphere/AppServer_8_5/bin/stopServer.bat ";
-        ProcessBuilder processBuilder = new ProcessBuilder(command, serverName);
+    public void stopServer(String serverId) {
+        String command = "C:/Desenvolvimento/IBM/WebSphere/AppServer_8_5/bin/stopServer.bat";
+        ProcessBuilder processBuilder = new ProcessBuilder(command, "server1", "-profileName", "AppSrv03");
         try {
             processBuilder.start();
-            System.out.println("Server " + serverName + " stopped.");
+            System.out.println("Server " + serverId + " stopped.");
         } catch (Exception e) {
-            System.err.println("Failed to stop server " + serverName + ": " + e.getMessage());
+            System.err.println("Failed to stop server " + serverId + ": " + e.getMessage());
         }
         // Process process = processes.get(serverName);
         // if (process != null) {
